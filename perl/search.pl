73dabf18920cf3dfab6abb29a824cff2958c87a1
LJ Gumby <eljaygumby@gmail.com>
2013-05-19 17:49:43 -0400

    fix regex

--- perl/search.pl
+++ perl/search.pl
@@ -5,7 +5,7 @@ $srcfile = "$ENV{DOCUMENT_ROOT}/../DCCC-LIII/800-53rev4controls-20130518.txt";
 # What are we looking for?
 $qstring = $ENV{QUERY_STRING};
 $qstring =~ s/find=//;
-$qstring =~ s/+/ /g;
+$qstring =~ s/[+]/ /g;
 $qstring =~ s/%([0-9A-Fa-f]{2})/pack("H2", $1)/ge;
 
 open($fh, "<", $srcfile) or die "cannot open $srcfile";
