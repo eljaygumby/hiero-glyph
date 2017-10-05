ec6d87ab90df46eaea4728be26b96ef533eeb180
LJ Gumby <eljaygumby@gmail.com>
2017-10-04 21:00:40 -0400

    work on merging v4 and v5

--- DCCC-LIII/merge2.pl
+++ DCCC-LIII/merge2.pl
@@ -2,11 +2,11 @@
 #
 # merge.pl - merge two 800-53 streams by control identifier
 #
-#
 
 # text types
 
-$texttype{title} = 0;
+$texttype{title} = -1;
+$texttype{withdrawn} = 0;
 $texttype{text} = 1;
 $texttype{supplement} = 2;
 $texttype{references} = 3;
@@ -14,7 +14,6 @@ $texttype{priority} = 4;
 $texttype{low} = 5;
 $texttype{mod} = 6;
 $texttype{high} = 7;
-$texttype{withdrawn} = 8;
 
 # compare control identifiers
 
@@ -26,9 +25,11 @@ sub compem {
   @a = split(/[:-]/, $a, 4);			# (control family, "number, etc.", type, text)
   @aa = split(/ /, @a[1], 2);			# (number within control family, the rest of the identifier)
   $aenhancement = 1 if ($aa[1] =~ /^[(]/);	# control enhancement?
+  $aenhancement = 2 if ($texttype{$a[2]} > 2);	# extra stuff goes after enhancements
   @b = split(/[:-]/, $b, 4);
   @bb = split(/ /, @b[1], 2);
   $benhancement = 1 if ($bb[1] =~ /^[(]/);
+  $benhancement = 2 if ($texttype{$a[2]} > 2);
 #print "a is $a control family is $a[0], number is $aa[0], enhancement is $aenhancement, type is $a[2]/$texttype{$a[2]}, the rest is x$aa[1]x\n";
 #print "b is $b control family is $b[0], number is $bb[0], enhancement is $benhancement, type is $b[2]/$texttype{$b[2]}, the rest is x$bb[1]x\n";
   if ($a[0] eq $b[0]) {					# control family
@@ -47,7 +48,7 @@ sub compem {
             }
           } else {
             return $aaa[1] <=> $bbb[1];
-          }
+          } 
         } else {
           if ($texttype{$a[2]} == $texttype{$b[2]}) {	# type
             return $aa[1] cmp $bb[1];			# the rest of the ID
